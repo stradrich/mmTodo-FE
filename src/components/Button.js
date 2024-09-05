f12146453dd68f0a26844f834d48492b809c1149
Aldrich <aldrichpinso@moneymatch.co>
2024-09-05 17:26:59 +0800

    feat: interactive createTask button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,14 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-export default function CreateTaskToggle({ handleClick }) {
+export default function CreateTaskToggle({ handleClick, task, handleTaskReset}) {
+  const createTask = () => {
+    console.log(`creating new task...${task}`);  
+    handleTaskReset();
+  }
   return (
     <Stack spacing={2} direction="row" sx={{ dislay: 'flex', justifyContent: 'end',}}>
-      <Button variant="contained" disabled>create</Button>
+      <Button variant="contained" disabled={task === ''} onClick={createTask}>create</Button>
       <Button sx={{ color: '#37e2ff' }} variant="text" onClick={ handleClick }>cancel</Button>
     </Stack>
   );
